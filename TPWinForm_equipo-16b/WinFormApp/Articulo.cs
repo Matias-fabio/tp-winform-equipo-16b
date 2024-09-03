422354b31c34cdf70be1651389604dc5d801b2ff
matias <maty.lufy@gmail.com>
2024-09-03 18:32:00 -0300

    Definicion base clase Marca y clase Categoria

--- TPWinForm_equipo-16b/WinFormApp/Articulo.cs
+++ TPWinForm_equipo-16b/WinFormApp/Articulo.cs
@@ -11,9 +11,8 @@ namespace WinFormApp
         public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-        //public Marca Marca { get; set; }
-
-        //public Categoria Categoria { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
 
         //public List<Imagen> Imagenes { get; set; }
         public float Precio { get; set; }
---
+++ TPWinForm_equipo-16b/WinFormApp/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormApp
+{
+    internal class Marca
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
